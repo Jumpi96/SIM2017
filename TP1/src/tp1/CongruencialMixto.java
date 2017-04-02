041b16ddf5542ccbc227354deec00b58c034dccd
MauriCasares <mauricd@hotmail.com>
2017-04-02 12:38:27 -0300

    Configuracion Optima

--- TP1/src/tp1/CongruencialMixto.java
+++ TP1/src/tp1/CongruencialMixto.java
@@ -1,5 +1,7 @@
 package tp1;
 
+import sun.security.krb5.JavaxSecurityAuthKerberosAccess;
+
 
 public class CongruencialMixto {
     private int a,c,m;
@@ -13,12 +15,35 @@ public class CongruencialMixto {
         this.k = k;
         this.g = g;
         this.seed = seed;
-    }          
+    }
+    
+    //Configuracion Optima
+    public CongruencialMixto() {
+        this.seed = 17;
+        this.a = 127;
+        this.c = (8 * this.a) + 3;        
+        this.m = (int)Math.pow(2, 50);
+    }
+
+    /*
+     · a debe ser un número impar, no divisible ni por 3 ni por 5.
+     · c usualmente puede ser cualquier constante, sin embargo, para asegurar
+     buenos resultados, se debe seleccionar a de tal forma que, a mod 8 = 5 para
+     una computadora binaria, o a mod 200 = 21 para computadora decimal.
+     · m debe ser el número entero más grande que la computadora acepte.
+     De acuerdo con Hull y Dobell, los mejores resultados para un generador
+     congruencial mixto en una computadora binaria son:
+     · c = 8*a±3
+     · a = cualquier entero
+     · X0 = Cualquier entero impar.
+     · M = 2^b donde b >2 y que m sea aceptado por la computadora.
+     
+     */
     
     //Retorna matriz con numeros en columna 0 y randoms en columna 1
     public Object[][] getNumeros(int cantidad){
-        Object[][] numeros= new Object[cantidad][2];
-        double xi=seed;
+        Object[][] numeros = new Object[cantidad][2];
+        double xi = this.seed;
         for (int i = 0; i < cantidad; i++) {
             xi=(a*xi+c)%m;
             numeros[i][0]=(int) xi;
--- TP1/src/tp1/TP1.java
+++ TP1/src/tp1/TP1.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tp1;
 
-/**
- *
- * @author juampilorenzo
- */
 public class TP1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         CongruencialMixto m = new CongruencialMixto(1001,1013,10000,0,0,9999);
         Object[][] array=m.getNumeros(20);
         for (int i = 0; i < array.length; i++) {

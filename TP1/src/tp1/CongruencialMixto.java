efd7fbbfe663261d38b6dad0a29fa13abdc33433
MauriCasares <mauricd@hotmail.com>
2017-04-02 13:02:06 -0300

    Configuracion Optima

--- TP1/src/tp1/CongruencialMixto.java
+++ TP1/src/tp1/CongruencialMixto.java
@@ -1,35 +1,40 @@
 package tp1;
 
-import sun.security.krb5.JavaxSecurityAuthKerberosAccess;
-
-
 public class CongruencialMixto {
-    private int a,c,m;
-    private int k,g;
+
+    private int a, c, m;
+    //private int k, g;
     private int seed;
 
-    public CongruencialMixto(int a, int c, int m, int k, int g, int seed) {
+    //Configuracion por usuario
+    /*public CongruencialMixto(int a, int c, int m, int k, int g, int seed) {
         this.a = a;
         this.c = c;
         this.m = m;
         this.k = k;
         this.g = g;
         this.seed = seed;
+    }*/
+
+    //Configuracion por usuario optima
+    public CongruencialMixto(int seed, int g, int a) {
+        this.seed = seed;
+        this.m = (int) Math.pow(2, g);
+        this.a = a;
+        this.c = (8 * this.a) + 3;
+        
     }
     
     //Configuracion Optima
     public CongruencialMixto() {
         this.seed = 17;
         this.a = 127;
-        this.c = (8 * this.a) + 3;        
-        this.m = (int)Math.pow(2, 50);
+        this.c = (8 * this.a) + 3;
+        this.m = (int) Math.pow(2, 15);
     }
 
     /*
      · a debe ser un número impar, no divisible ni por 3 ni por 5.
-     · c usualmente puede ser cualquier constante, sin embargo, para asegurar
-     buenos resultados, se debe seleccionar a de tal forma que, a mod 8 = 5 para
-     una computadora binaria, o a mod 200 = 21 para computadora decimal.
      · m debe ser el número entero más grande que la computadora acepte.
      De acuerdo con Hull y Dobell, los mejores resultados para un generador
      congruencial mixto en una computadora binaria son:
@@ -37,17 +42,19 @@ public class CongruencialMixto {
      · a = cualquier entero
      · X0 = Cualquier entero impar.
      · M = 2^b donde b >2 y que m sea aceptado por la computadora.
-     
      */
     
+    
     //Retorna matriz con numeros en columna 0 y randoms en columna 1
-    public Object[][] getNumeros(int cantidad){
+    public Object[][] getNumeros(int cantidad) {
         Object[][] numeros = new Object[cantidad][2];
+
         double xi = this.seed;
+
         for (int i = 0; i < cantidad; i++) {
-            xi=(a*xi+c)%m;
-            numeros[i][0]=(int) xi;
-            numeros[i][1]=xi/(m-1);
+            xi = (a * xi + c) % m;
+            numeros[i][0] = (int) xi;
+            numeros[i][1] = xi / (m - 1);
         }
         return numeros;
     }
--- TP1/src/tp1/TP1.java
+++ TP1/src/tp1/TP1.java
@@ -3,11 +3,24 @@ package tp1;
 public class TP1 {
 
     public static void main(String[] args) {
-        CongruencialMixto m = new CongruencialMixto(1001,1013,10000,0,0,9999);
-        Object[][] array=m.getNumeros(20);
+        //CongruencialMixto m = new CongruencialMixto(1001,1013,10000,0,0,9999);
+        
+        CongruencialMixto m = new CongruencialMixto(1001,1013,9999);
+        CongruencialMixto n = new CongruencialMixto();
+        
+        Object[][] array=m.getNumeros(100);
+        
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i][0]+"/"+array[i][1]);
+            //System.out.println(array[i][0]+"/" + (double)array[i][1] * 100);
+            System.out.println((int)((double)array[i][1] * 100));
+        }
+        
+        System.out.println("");
+        System.out.println("");
+        Object[][] arreglo = n.getNumeros(100);
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println((int)((double)arreglo[i][1] * 100));
         }
     }
-    
 }

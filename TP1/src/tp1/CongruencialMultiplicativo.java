a9ad81bed1eb7f06c9866607e50af6b5112aae3a
matiasgiro <matiasgiro@gmail.com>
2017-04-06 16:52:33 -0300

    congruencial multiplicativo

---
+++ TP1/src/tp1/CongruencialMultiplicativo.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tp1;
+
+/**
+ *
+ * @author Juan Matias
+ */
+public class CongruencialMultiplicativo {
+    private long a, c, m;
+    private int seed;
+    
+    public CongruencialMultiplicativo(int seed, int m, int a, int c) {
+        this.seed = seed;
+        this.m = m;
+        this.a = a;
+        this.c = c;
+    }
+
+    public long getA() {
+        return a;
+    }
+
+    public void setA(long a) {
+        this.a = a;
+    }
+
+    public long getC() {
+        return c;
+    }
+
+    public void setC(long c) {
+        this.c = c;
+    }
+
+    public long getM() {
+        return m;
+    }
+
+    public void setM(long m) {
+        this.m = m;
+    }
+
+    public int getSeed() {
+        return seed;
+    }
+
+    public void setSeed(int seed) {
+        this.seed = seed;
+    }
+    
+    
+    
+    //Configuracion Optima
+    public CongruencialMultiplicativo() {
+        this.seed = 127;
+        this.m = (long) Math.pow(2, 32);
+        this.a = 1 + (416131 * 8);
+        this.c = 999983;
+        
+        /*
+        Hull-Dobell Theorem:
+        
+        m and the offset,c are relatively prime
+        a-1 is divisible by all prime factors of m,
+        a-1 is divisible by 4 if m is divisible by 4.
+        */
+    }
+    
+    //Retorna matriz con numeros en columna 0 y randoms en columna 1
+    public Object[][] getNumeros(int cantidad) {
+        Object[][] numeros = new Object[2][cantidad];
+
+        double xi = this.seed;
+
+        for (int i = 0; i < cantidad; i++) {
+            xi = (a * xi) % m;
+            numeros[0][i] = (long) xi;
+            numeros[1][i] = xi / (m - 1);
+        }
+        return numeros;
+    }
+}
+    
+

af5346989405660066b6be8df5135ae4d217f084
juampilorenzo <juampilorenzo@gmail.com>
2017-06-01 20:02:20 -0300

    Metodos terminados sin probar

--- TP5/TP5/src/tp5/Simulacion.java
+++ TP5/TP5/src/tp5/Simulacion.java
@@ -31,11 +31,14 @@ public class Simulacion {
     */
     
     private ArrayList<Object[]> tablaMostrar;
+    private ArrayList<Object[]> colaCarniceria;
+    private ArrayList<Object[]> colaFiambreria;
     private Object[][] tabla;
     private int dias;
     private int horas;
     private int mostrarDesde;
     private int mostrarHasta;
+    private double acumuladorEspera=0.0;
 
     public Simulacion(int dias,int horas,int diasDesde, int diasHasta) {
         this.dias=dias;
@@ -55,43 +58,165 @@ public class Simulacion {
     
     public ArrayList<Object[]> simular(){
         Object[] row;
+        Object[] temp;
         for (int i = 0; i < dias; i++) {
-            while((Double)tabla[1][1] < horas){
+            while((!tabla[1][0].equals("Fin del dia"))){
                 row = new Object[24];
                 row[0] = getEstado();
                 row[1] = getProximo();
-                
                 if(esLlegada((float)row[1])){
                     row[2] = (float)Math.random();
                     row[3] = getTiempoLlegada(0.5F, (float)row[2]);
                     row[4] = (float)row[1] + (float)row[3];
                     row[5] = (float) Math.random();
-                    row[6] = addCliente(row[5]); // agregas el cliente a la otra tabla
+                    row[6] = addCliente((double)row[5],(double)row[1]);
+                    
+                    if(row[6].equals("Carniceria")&&
+                            (!tabla[1][18].equals("Atendiendo"))){
+                        row[7] = (float) Math.random();
+                        row[8] = getDemoraCarniceria((float)row[7]);
+                        row[9]= (float) Math.random();
+                        row[10]= getDemoraTotalCarniceria((float)row[8], (float)row[9]);
+                        row[11]= tabla[1][11];
+                        row[12] = "-";
+                        row[13] = "-";
+                        row[14] = "-";
+                        row[15] = "-";
+                        row[16] = tabla[1][16];
+                        row[17] = "-";
+                        row[18] = getCliente1(row);
+                        row[19] = getFinAtencionCliente1(row);
+                        row[20]=tabla[1][20];
+                        row[21] = "-";
+                        row[22] = (Integer)tabla[1][22]+1;
+                        acumuladorEspera+=(double)row[19]-(double)row[1];
+                        row[23] = acumuladorEspera; 
+                        colaCarniceria.remove(0);
+                    }
+                    else if(row[6].equals("Fiambreria") &&
+                            (!tabla[1][20].equals("Atendiendo"))){
+                        row[7]="-";
+                        row[8]="-";
+                        row[9]="-";
+                        row[10]="-";
+                        row[11]=tabla[1][11];
+                        row[12] = (float)Math.random();
+                        row[13] = getDemoraFiambreria((float)row[12]);
+                        row[14] = (float)Math.random();
+                        row[15] = getDemoraTotalFiambreria((float)row[13], (float)row[14]);
+                        row[16] = tabla[1][16];
+                        row[17] = "-";
+                        row[18]=tabla[1][18];
+                        row[19]="-";
+                        row[20]=getCliente2(row);
+                        row[21]=getFinAtencionCliente2(row);
+                        row[22]=(Integer)tabla[1][22]+1;
+                        acumuladorEspera+=(double)row[21]-(double)row[1];
+                        row[23]=acumuladorEspera;
+                        colaFiambreria.remove(0);
+                    }
+                    else if(row[6].equals("Verduleria") && 
+                            (!tabla[1][20].equals("Atendiendo")
+                             || !tabla[1][18].equals("Atendiendo"))){
+                        if(!tabla[1][18].equals("Atendiendo")){
+                            row[7]="-";
+                            row[8]="-";
+                            row[9]="-";
+                            row[10]="-";
+                            row[11]=tabla[1][11];
+                            row[12]="-";
+                            row[13]="-";
+                            row[14]="-";
+                            row[15]="-";
+                            row[16]=tabla[1][16];
+                            row[17]=0.2/60;
+                            row[18]=getCliente1(row);
+                            row[19]=getFinAtencionCliente1(row);
+                            row[20]=tabla[1][20];
+                            row[21]=tabla[1][21];
+                            row[22]=(Integer)tabla[1][22]+1;
+                            acumuladorEspera+=(double)row[19]-(double)row[1];
+                            row[23]=acumuladorEspera;
+                            
+                            row[6]="Verduleria (C)";
+                            colaCarniceria.remove(0);
+                        }
+                        else{
+                            row[7]="-";
+                            row[8]="-";
+                            row[9]="-";
+                            row[10]="-";
+                            row[11]=tabla[1][11];
+                            row[12]="-";
+                            row[13]="-";
+                            row[14]="-";
+                            row[15]="-";
+                            row[16]=tabla[1][16];
+                            row[17]=0.2/60;
+                            row[18]=tabla[1][20];
+                            row[19]=tabla[1][21];
+                            row[20]=getCliente2(row);
+                            row[21]=getFinAtencionCliente2(row);
+                            row[22]=(Integer)tabla[1][22]+1;
+                            acumuladorEspera+=(double)row[21]-(double)row[1];
+                            row[23]=acumuladorEspera;
+                            
+                            row[6]="Verduleria (F)";
+                            colaFiambreria.remove(0);
+                        }
+                    }
+                    else{
+                        row[7] = "-";
+                        row[8] = "-";
+                        row[9] = "-";
+                        row[10] = "-";
+                        row[11] = getColaCarniceria(null);
+                        row[12] = "-";
+                        row[13] = "-";
+                        row[14] = "-";
+                        row[15] = "-";
+                        row[16] = getColaFiambreria(null);
+                        row[17] = "-";
+                        row[18]=tabla[1][18];
+                        row[19]=tabla[1][19];
+                        row[20]=tabla[1][20];
+                        row[21]=tabla[1][21];
+                        row[22] = (Integer)tabla[1][22]+1;
+                        row[23] = acumuladorEspera;  
+                    }
+                }
+                else if(row[1].equals("Fin del dia")){
+                    row[2] = "-";
+                    row[3] = "-";
+                    row[4] = "-";
+                    row[5] = "-";
+                    row[6] = "-";
                     row[7] = "-";
                     row[8] = "-";
                     row[9] = "-";
                     row[10] = "-";
-                    row[11] = getColaCarniceria(row);
+                    row[11] = "-";
                     row[12] = "-";
                     row[13] = "-";
                     row[14] = "-";
                     row[15] = "-";
-                    row[16] = getColaFiambreria(row);
+                    row[16] = "-";
                     row[17] = "-";
-                    row[18] = tabla[1][18];
+                    row[18] = "Libre";
                     row[19] = "-";
-                    row[20] = tabla[1][20];
+                    row[20] = "Libre";
                     row[21] = "-";
-                    row[22] = (Integer)tabla[1][22]+1;
-                    row[23] = tabla[1][23];  
+                    row[22] = (Integer)tabla[1][22];
+                    row[23] = acumuladorEspera;  
                 }
                 else{
                     row[2]="-";
                     row[3]="-";
                     row[4]=tabla[1][4];
                     row[5]="-";
-                    row[6] = getProxCliente(row[1]); // le pasas el reloj para que lo busque
-                    if (row[6].equals("Carnicería")){
+                    temp=getProxCliente((double)row[1]);
+                    row[6] = temp[0];
+                    if (row[6].equals("Carniceria")){
                         row[7] = (float) Math.random();
                         row[8] = getDemoraCarniceria((float)row[7]);
                         row[9]= (float) Math.random();
@@ -101,16 +226,18 @@ public class Simulacion {
                         row[13]="-";
                         row[14]="-";
                         row[15]="-";
-                        row[16]="-";
+                        row[16]=tabla[1][16];
                         row[17]="-";
                         row[18]=getCliente1(row);
                         row[19]=getFinAtencionCliente1(row);
                         row[20]=tabla[1][20];
                         row[21]="-";
                         row[22]=(Integer)tabla[1][22];
-                        row[23]=getTiempoEspera(row);
+                        acumuladorEspera+=(double)row[19]-(double)temp[1];
+                        row[23]=acumuladorEspera;
+                        colaCarniceria.remove(0);
                     }
-                    else if(row[6].equals("Fiambrería")){
+                    else if(row[6].equals("Fiambreria")){
                         row[7]="-";
                         row[8]="-";
                         row[9]="-";
@@ -127,26 +254,51 @@ public class Simulacion {
                         row[20]=getCliente2(row);
                         row[21]=getFinAtencionCliente2(row);
                         row[22]=(Integer)tabla[1][22];
-                        row[23]=getTiempoEspera(row);
+                        acumuladorEspera+=(double)row[21]-(double)temp[1];
+                        row[23]=acumuladorEspera;
+                        colaFiambreria.remove(0);
                     }
                     else{
-                        row[7]="-";
-                        row[8]="-";
-                        row[9]="-";
-                        row[10]="-";
-                        row[11]=getColaCarniceria(row);
-                        row[12]="-";
-                        row[13]="-";
-                        row[14]="-";
-                        row[15]="-";
-                        row[16]=getColaFiambreria(row);
-                        row[17]=0.2/60;
-                        row[18]=tabla[1][18];
-                        row[19]="-";
-                        row[20]=tabla[1][20];
-                        row[21]="-";
-                        row[22]=(Integer)tabla[1][22];
-                        row[23]=getTiempoEspera(row);
+                        if (row[6].equals("Verduleria (C)")){
+                            row[7]="-";
+                            row[8]="-";
+                            row[9]="-";
+                            row[10]="-";
+                            row[11]=getColaCarniceria(row);
+                            row[12]="-";
+                            row[13]="-";
+                            row[14]="-";
+                            row[15]="-";
+                            row[16]=tabla[1][16];
+                            row[17]=0.2/60;
+                            row[18]=getCliente1(row);
+                            row[19]="-";
+                            row[20]=tabla[1][20];
+                            row[21]="-";
+                            row[22]=(Integer)tabla[1][22];
+                            acumuladorEspera+=(double)row[19]-(double)temp[1];
+                            row[23]=acumuladorEspera;
+                        }
+                        else{
+                            row[7]="-";
+                            row[8]="-";
+                            row[9]="-";
+                            row[10]="-";
+                            row[11]=tabla[1][11];
+                            row[12]="-";
+                            row[13]="-";
+                            row[14]="-";
+                            row[15]="-";
+                            row[16]=getColaFiambreria(row);
+                            row[17]=0.2/60;
+                            row[18]=tabla[1][18];
+                            row[19]="-";
+                            row[20]=getCliente2(row);
+                            row[21]="-";
+                            row[22]=(Integer)tabla[1][22];
+                            acumuladorEspera+=(double)row[21]-(double)temp[1];
+                            row[23]=acumuladorEspera;
+                        }
                     }
                 }
 
@@ -157,10 +309,10 @@ public class Simulacion {
                 tabla[1][11]=row[11];
                 tabla[1][16]=row[16];
                 tabla[1][18]=row[18];
-                if(!row[19].equals("-"))
+                if(row[19].equals("-"))
                     tabla[1][19]=row[19];
                 tabla[1][20]=row[20];
-                if(!row[21].equals("-"))
+                if(row[21].equals("-"))
                     tabla[1][21]=row[21];
                 tabla[1][22]=row[22];
                 tabla[1][23]=row[23];
@@ -172,7 +324,124 @@ public class Simulacion {
         }
         return tablaMostrar;
     }
+    private String getColaCarniceria(Object[] obj){
+        String cadena="";
+        Object[] actual;
+        boolean primero=true;
+        for (int i = 0; i < colaCarniceria.size(); i++) {
+            if (primero && obj!=null){
+                primero=false;
+                continue;
+            }
+            actual=colaCarniceria.get(0);
+            if (actual[0].equals("Carnicería"))
+                cadena+="C ";
+            else
+                cadena+="V ";
+        }
+        return cadena;
+    }
+    private String getColaFiambreria(Object[] obj){
+        String cadena="";
+        Object[] actual;
+        boolean primero=true;
+        for (int i = 0; i < colaCarniceria.size(); i++) {
+            if (primero && obj!=null){
+                primero=false;
+                continue;
+            }
+            actual=colaCarniceria.get(0);
+            if (actual[0].equals("Fiambrería"))
+                cadena+="F ";
+            else
+                cadena+="V ";
+        }
+        return cadena;
+    }
+    private double getFinAtencionCliente1 (Object[] row){
+        return (double)row[1]+(double)row[10];
+    }
+    private double getFinAtencionCliente2 (Object[] row){
+        return (double)row[1]+(double)row[15];
+    }
+    private String getCliente1(Object[] row){
+        if(row[0].equals("Inicio del dia"))
+            return "Libre";
+        else if(!row[10].equals("-")
+                || !row[17].equals("-"))
+            return "Atendiendo";
+        else
+            return "Libre";
+    }
+    private String getCliente2(Object[] row){
+        if(row[0].equals("Inicio del dia"))
+            return "Libre";
+        else if(!row[15].equals("-")
+                || !row[17].equals("-"))
+            return "Atendiendo";
+        else
+            return "Libre";
+    }
+    private String getEstado(){
+        double proxLlegada=(double)tabla[1][4];
+        double finCarniceria=(double)tabla[1][19];
+        double finFiambreria=(double)tabla[1][21];
+        boolean finHora = (double)tabla[1][1]>horas;
+        if(tabla[0][0].equals(""))
+            return "Inicio del dia";
+        else{
+            if (!finHora){
+                if(proxLlegada<finCarniceria && proxLlegada<finFiambreria)
+                    return("Llegada cliente");
+                else if(finCarniceria<proxLlegada && finCarniceria<finFiambreria)
+                    return("Fin Atencion C");
+                else
+                    return("Fin Atencion F");
+            }
+            else{
+                if(finCarniceria<finFiambreria)
+                    if(colaCarniceria.isEmpty() && tabla[1][18].equals("Libre"))
+                        if(tabla[1][20].equals("Atendiendo"))
+                            return "Fin Atencion F";
+                        else
+                            return "Fin del dia";
+                    else
+                        return "Fin Atencion C";
+                else
+                    if(colaFiambreria.isEmpty() && tabla[1][20].equals("Libre"))
+                        if(tabla[1][18].equals("Atendiendo"))
+                            return "Fin Atencion C";
+                        else
+                            return "Fin del dia";
+                    else
+                        return "Fin Atencion F";
+            }     
+        }
+    }
     
+    private String addCliente(double rnd,double reloj){
+        String tipo=getTipoCliente(rnd);
+        Object[] row= {tipo,reloj};
+        if(tipo.equals("Carniceria"))
+            colaCarniceria.add(row);
+        else if(tipo.equals("Fiambreria"))
+            colaFiambreria.add(row);
+        else
+            if(colaCarniceria.size()<=colaFiambreria.size())
+                colaCarniceria.add(row);
+            else
+                colaFiambreria.add(row);
+        
+        return tipo;
+    }
+    private String getTipoCliente(double rnd){
+        if (rnd<0.30)
+            return "Verdulería";
+        else if(rnd<0.56)
+            return "Carnicería";
+        else
+            return "Fiambrería";
+    }
     //Tengo que ver en la tabla cual de los 3 eventos se produce antes (llagada o fin de atención 1 o 2)
     private float getProximo(){
         
@@ -262,6 +531,7 @@ public class Simulacion {
     private void inicializarTabla(){
         this.tabla=new Object[2][24];  
         
+        this.tabla[0][1] = "";
         this.tabla[1][1] = 0.0F;
         this.tabla[1][2] = (float)Math.random();
         this.tabla[1][3] = getTiempoLlegada(0.5F, (float)this.tabla[1][2]);
@@ -285,7 +555,25 @@ public class Simulacion {
         this.tabla[1][21] = 0;
         this.tabla[1][22] = 0;
     }
-    
+    private Object[] getProxCliente(double reloj){
+        Object[] r;
+        if(reloj==(double)tabla[1][19]){
+            r=colaCarniceria.get(0);
+            if (r[0].equals("Verduleria")){
+                colaCarniceria.remove(0);
+                return new Object[]{"Verduleria (C)",r[1]};
+            }
+        }
+        else{
+            r=colaFiambreria.get(0);
+            if (r[0].equals("Verduleria")){
+                colaFiambreria.remove(0);
+                return new Object[]{"Verduleria (F)",r[1]};
+            }
+                
+        }        
+        return r;
+    }
         /*
     [0]: Estado.
     [1]: Reloj.
